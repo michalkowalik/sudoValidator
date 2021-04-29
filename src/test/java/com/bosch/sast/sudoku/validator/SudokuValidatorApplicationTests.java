7f375eefd9b70b97e7611a0e2802fcd15266beec
Michal Kowalik <MichalPawel.Kowalik@de.bosch.com>
2021-04-29 23:27:44 +0200

    initial commit

---
+++ src/test/java/com/bosch/sast/sudoku/validator/SudokuValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosch.sast.sudoku.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
